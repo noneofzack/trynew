97c10ba0e0475e5d937b46fc0ce47ff3b1aeba9f
noneofzack <giftalexa1228@gmail.com>
2024-11-16 11:03:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -116,7 +116,7 @@ def approval():
     print(f"\033[1;37m [\u001b[36m•\033[1;37m] Your Key :\u001b[36m {key}")
     
     urls = [
-        "https://github.com/nathanielromerorfcp/approval/blob/main/PremiumTools/approval.txt"
+        "https://github.com/noneofzack/approval/blob/main/approval.txt"
     ]
     
     key_found = False
